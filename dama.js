0395898ecbdbafc3847482b1b619f8be4053a2c5
Kennargraph <25281909+TH68JDUP@users.noreply.github.com>
2022-12-01 12:45:24 +0800

    Update and rename dama.js to dama.plugin

--- dama.js
+++
@@ -1,4 +0,0 @@
-[MITM]
-hostname = sdk.yunduoketang.com
-[Script]
-http-response http://sdk.yunduoketang.com/appApi/course/liveAndFace/list requires-body=1,script-path= https://raw.githubusercontent.com/sngxpro/QuantumultX/master/dama/dama.js
