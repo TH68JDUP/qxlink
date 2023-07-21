08c07819ae0eafab76d84b8d07479df7e598d8e9
Kennargraph <25281909+TH68JDUP@users.noreply.github.com>
2023-07-21 02:44:28 +0000

    Update moxe2.js

--- jscript/moxe2.js
+++ jscript/moxe2.js
@@ -1,4 +1,4 @@
-**************************************
+/**************************************
 
 [rewrite_local]
 ^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/MOZE.js
