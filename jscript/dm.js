bf8664623dc730ec80341cd48ad53db7eae7b454
Kennargraph <25281909+TH68JDUP@users.noreply.github.com>
2023-07-21 01:38:03 +0000

    Update dm.js

--- jscript/dm.js
+++ jscript/dm.js
@@ -1,10 +1,5 @@
 /*
-微信公众号：少年歌行PRO 制作
-电报资源发布群（禁言纯净）@shao66 
-电报水友交流群（资源同步）@sngxpro
-电报私聊 @shaogebot
-QQ群：654371081
-圈X配置：
+
 [rewrite_local]
 ^http?:\/\/sdk\.yunduoketang\.com\/appApi\/course\/liveAndFace\/list url script-response-body https://raw.githubusercontent.com/sngxpro/QuantumultX/master/dama/dama.js
 [mitm]
